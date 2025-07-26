f46dc4a12dfdfb0daace91532a43e52c5296f746
Felix140301 <orgafelixcosmin@gmail.com>
2025-07-26 19:12:23 +0300

    Build

---
+++ spy-game/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig = {
+  basePath: "/SpyGame",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+export default nextConfig;
